47a442e6cc0e91806836aa451b3fb557c72f0dd3
desirekaleba <desirekaleba@gmail.com>
2020-04-13 14:30:36 +0300

    app.js ini

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import { estimatorRouter } from './routes/estimator.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api/v1/on-covid-19/', estimatorRouter);
+
+
+export default app;
