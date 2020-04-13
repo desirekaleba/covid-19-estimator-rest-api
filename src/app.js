6727f66b7dc70739530cfd03b816b8302d37091a
desirekaleba <desirekaleba@gmail.com>
2020-04-13 23:18:45 +0300

    ready for deployment

--- src/app.js
+++ src/app.js
@@ -8,6 +8,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use('/api/v1/on-covid-19/', estimatorRouter);
 
 app.get('/', (req, res) => {
