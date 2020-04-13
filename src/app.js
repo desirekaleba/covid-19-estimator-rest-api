e42bdd6ce41a0374f470f2a7f97a7dfb1889c30e
desirekaleba <desirekaleba@gmail.com>
2020-04-13 20:01:14 +0300

    ready for deployment

--- src/app.js
+++ src/app.js
@@ -12,9 +12,8 @@ app.use('/api/v1/on-covid-19/', estimatorRouter);
 
 app.get('/', (req, res) => {
   res.status(200).send(`Welcome to covid19 REST api, 
-    you can POST json data and i will respond 
-    with either json or xml if you hits /api/v1/on-covid-19/json`);
+    you can POST json data and i will respond
+    with covid19 data estimation either in json format or xml if you hits /api/v1/on-covid-19/json`);
 });
-
 
 export default app;
