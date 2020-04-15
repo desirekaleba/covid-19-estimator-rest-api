af7b65dd0481643fc8812ba118c4c3842faac190
desirekaleba <desirekaleba@gmail.com>
2020-04-15 21:36:36 +0300

    dist-server removed

--- jest.config.js
+++ jest.config.js
@@ -6,16 +6,8 @@ module.exports = {
   coveragePathIgnorePatterns: [
     'node_modules',
     'coverage',
-    'src/index.js',
-    'src/app.js',
     'src/logger/logger.js',
-    'src/routes/estimator.js'
-  ],
-  moduleNameMapper: {
-    // translate all your custom paths here, read the doc in the link above
-    '^@finder/(.*)$': '<rootDir>/files-manipulation/$1',
-    '^@metadata/(.*)$': '<rootDir>/folder-metadata/$1',
-    '^@logger/(.*)$': '<rootDir>/logging/$1'
-    // ...and so on
-  }
+    'src/routes/estimator.js',
+    'src/routes/index.js'
+  ]
 };
