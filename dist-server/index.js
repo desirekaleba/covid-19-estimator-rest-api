d8c0711ff2e44583e2050f62068c029e1cdd58d7
desirekaleba <desirekaleba@gmail.com>
2020-04-15 21:32:08 +0300

    dist-server removed

--- dist-server/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-const PORT = process.env.PORT || 5000;
-
-_app.default.listen(PORT);
